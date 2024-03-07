7afc5b2dfe2e3c1bf87b0bd49aa7bd60d65f7c27
R-Newson <yearnnewsonogweno@gmail.com>
2024-03-07 15:55:35 +0300

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#Ask the user for their name
+name = input("What is your name? ")
+
+#Ask the user for their age
+age = input("What is your age? ")
+
+#Ask the user for their location
+location = input("What is your location? ")
+
+#Print out a personalised message
+print(f"Hello, {name}! It is a pleasure to know that you are {age} years old and lives in {location}. Nice to meet you! ")
